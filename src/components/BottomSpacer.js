db507f92d49e09552eea90651314791bc1d4eb39
Joe-EtubiGreatattai <greatattai442442@gmail.com>
2024-11-23 03:31:06 +0100

    spacer fixed

--- src/components/BottomSpacer.js
+++ src/components/BottomSpacer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const BottomSpacer = () => {
-  return <div style={{ height: '64px' }} />; // 64px matches bottom nav height (padding + icon size)
+  return <div style={{ height: '164px' }} />; // 64px matches bottom nav height (padding + icon size)
 };
 
 export default BottomSpacer;
