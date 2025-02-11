736d9118f69ff2ac8bf7f4be3952c123a1a9363f
Joe-Etubi Great Attai <greatattai442442@gmail.com>
2025-02-11 02:18:18 +0100

    feat: update manifest URL in ConnectWallet component for improved connectivity

--- src/components/ConnectWallet.js
+++ src/components/ConnectWallet.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { TonConnect } from '@tonconnect/sdk';
 
 const tonConnect = new TonConnect({
-  manifestUrl: 'https://hashtag-weld.vercel.app/tonconnect-manifest.json', // Update with your hosted manifest
+  manifestUrl: 'https://api.hashtagdigital.net/tonconnect-manifest.json', // Update with your hosted manifest
 });
 
 console.log('tonConnect:', tonConnect);
